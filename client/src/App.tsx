74a91b66c98ab6a4c8b5db65c5cbf16e865a4463
vtran <vytran1805@gmail.com>
2023-12-21 00:20:16 -0500

    Initial structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,32 @@
-import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { useMemo } from "react";
 import { themeSettings } from "./theme";
-// import { BrowserRouter } from "react-router-dom";
-import ErrorBoundary from "./ErrorBoundary";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "@/pages/Signup";
+import Account from "@/pages/Account";
+import { HomePage } from "@/pages/HomePage";
+import { NavBar } from "@/components/Navbar";
+import { Login } from "@/pages/Login";
 
 function App() {
   // create theme that comes from MUI
+  // const [isLoginOpen, setIsLoginOpen] = useState(false);
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-      {/* <ErrorBoundary> */}
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline will reset the setting of MUI to default */}
-        <CssBaseline />
-        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-          This is the box
-        </Box>
-      </ThemeProvider>
-      {/* </ErrorBoundary> */}
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline will reset the setting of MUI to default */}
+          <CssBaseline />
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Signup />} />
+            <Route path="/account" element={<Account />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
