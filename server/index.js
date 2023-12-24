a6ad17ce45085a939709f0dc66e2572e35497bc4
vtran <vytran1805@gmail.com>
2023-12-24 13:22:48 -0500

    Refactor

--- server/index.js
+++ server/index.js
@@ -1,91 +1,95 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const body_parser_1 = __importDefault(require("body-parser"));
-const mongoose_1 = __importDefault(require("mongoose"));
-const cors_1 = __importDefault(require("cors"));
-const bcrypt_1 = __importDefault(require("bcrypt"));
-const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-const userSchema_1 = __importDefault(require("./models/userSchema")); // Assuming UserDocument is a Mongoose document type
-const dotenv_1 = __importDefault(require("dotenv"));
+import express from 'express';
+import bodyParser from 'body-parser'; //handle information that’s coming from the body of a request
+import mongoose from 'mongoose';
+import cors from 'cors'; //handle cross origin resource sharing requests so that we can call from different URL
+import bcrypt from 'bcrypt'; //Library used for securely hashing passwords, aiding in storing passwords securely by making them difficult to reverse engineer.
+import jwt from 'jsonwebtoken'; //Package allowing the generation and verification of JSON Web Tokens (JWTs), commonly used for authentication and secure transmission of information between parties.
+import User from './models/userSchema.js';
+import dotenv from 'dotenv';
 /* CONFIGURATION */
-dotenv_1.default.config();
+dotenv.config();
 const PORT = process.env.PORT || 8080;
+
 /* CONNECT TO EXPRESS APP */
-const app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.use(body_parser_1.default.urlencoded({ extended: false }));
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 /* CONNECT TO MONGODB */
-mongoose_1.default
-    .connect(process.env.MONGO_URL)
-    .then(() => {
+// NOTE: useNewUrlParser, useUnifiedTopology, useFindAndModify, and useCreateIndex are no longer supported options.
+// Mongoose 6 always behaves as if useNewUrlParser, useUnifiedTopology, and useCreateIndex are true, and useFindAndModify is false
+// See: https://mongoosejs.com/docs/migrating_to_6.html#no-more-deprecation-warning-options
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
     app.listen(PORT, () => {
-        console.log(`Server is connected to port ${PORT} and connected to MongoDB`);
+      console.log(
+        `Server is connected to port ${PORT} and connected to MongoDB`
+      );
     });
-})
-    .catch((error) => {
+  })
+  .catch((error) => {
     console.log('Unable to connect to Server and/or MongoDB');
-});
+  });
+
 /* MIDDLEWARE */
-app.use((0, cors_1.default)());
-app.use(body_parser_1.default.json());
+app.use(cors());
+app.use(bodyParser.json());
+
 /* SCHEMA */
 // userSchema
+
 /* ROUTES */
 // POST users
-app.post('/register', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const { email, username, password } = req.body;
-        const hashedPassword = yield bcrypt_1.default.hash(password, 10);
-        const newUser = new userSchema_1.default({ email, username, password: hashedPassword });
-        yield newUser.save();
-        res.status(201).json({ message: '201 - User created successfully' });
-    }
-    catch (error) {
-        res.status(500).json({ error: '500 - Internal Server Error' });
-    }
-}));
+app.post('/register', async (req, res) => {
+  try {
+    const { email, username, password } = req.body;
+    const hashedPasword = await bcrypt.hash(password, 10); //create a hashed password: 10 is no. of rounds, higher numbers increase security but also take more time to generate the hash.
+    const newUser = new User({ email, username, password: hashedPasword }); //create new userSchema
+    await newUser.save(); //save to the database
+    res.status(201).json({ message: '201 - User created successfully' }); //return the resquest status when new user is successfully created
+  } catch (error) {
+    res.status(500).json({ eror: '500 - Internal Server Error' });
+  }
+});
 // GET registered users
-app.get('/register', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const users = yield userSchema_1.default.find();
-        res.status(201).json(users);
-    }
-    catch (error) {
-        res.status(500).json({ error: '500 - Unable to get users' });
-    }
-}));
+app.get('/register', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(201).json(users);
+  } catch (error) {
+    res.status(500).json({ eror: '500 - Unable to get users' });
+  }
+});
+
 // GET login user
-app.post('/login', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const { username, password } = req.body;
-        const user = yield userSchema_1.default.findOne({ username });
-        if (!user) {
-            return res.status(401).json({ error: '401 - Invalid credentials' });
-        }
-        const isPasswordValid = yield bcrypt_1.default.compare(password, user.password);
-        if (!isPasswordValid) {
-            return res.status(401).json({ error: '401 - Invalid password' });
-        }
-        const token = jsonwebtoken_1.default.sign({ userId: user._id }, process.env.SECRET_KEY, {
-            expiresIn: '1hr',
-        });
-        res.json({ message: 'Login successful' });
+// Endpoint for user login
+app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body; // Extract username and password from request body
+    const user = await User.findOne({ username }); // Find user in the database based on the provided username
+
+    if (!user) {
+      return res.status(401).json({ error: '401 - Invalid credentials' });
     }
-    catch (error) {
-        res.status(500).json({ error: '500 - Unable to login', message: error });
+
+    const isPasswordValid = await bcrypt.compare(password, user.password); // verify provided password against the user's stored hashed password
+
+    if (!isPasswordValid) {
+      // If password is invalid, return error for invalid password
+      return res.status(401).json({ error: '401 - Invalid password' });
     }
-}));
-// app.listen(PORT);
+
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+      // Generate a JWT token with user ID payload for successful login, session expires in 1hr
+      expiresIn: '1hr',
+    });
+
+    res.json({ message: 'Login successful' });
+    // res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: '500 - Unable to login', message: error });
+  }
+});
+
+// app.listen(PORT); //use port 3001
