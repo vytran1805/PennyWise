a3933200d1074ec901e55d14b46fd809b2225cbb
vtran <vytran1805@gmail.com>
2023-12-25 13:20:42 -0500

    Add messages for transaction Schema

--- server/models/transaction.js
+++ server/models/transaction.js
@@ -1,16 +1,16 @@
 import mongoose from 'mongoose';
 
 const transactionSchema = new mongoose.Schema({
-  name: { type: String, require: true },
+  name: { type: String, require: [true, 'Name is required'] },
   description: { type: String, require: false },
-  amount: { type: Number, require: true },
+  amount: { type: Number, require: [true, 'Amount is required'] },
   date: { type: mongoose.Schema.Types.Date, require: false },
   // category: {
   //   type: mongoose.Schema.Types.ObjectId,
   //   ref: 'Category',
   //   required: true,
   // },
-  category: { type: String, required: true },
+  category: { type: String, required: [true, 'Category is required'] },
 });
 
 const Transaction = mongoose.model('Transaction', transactionSchema);
