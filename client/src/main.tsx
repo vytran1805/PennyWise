5a1abb7f85c5e794f1c5f32f08abc702fe582b30
vtran <vytran1805@gmail.com>
2023-12-27 13:50:46 -0500

    Pass in Provider redux

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import ErrorBoundary from "./ErrorBoundary.tsx";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import ErrorBoundary from './ErrorBoundary.tsx';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.ts';
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <Provider store={store}>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </Provider>
   </React.StrictMode>
 );
