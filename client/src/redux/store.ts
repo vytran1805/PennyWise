006d506ec201594036681c6268e8239e53e8d9bf
vtran <vytran1805@gmail.com>
2024-01-02 19:43:52 -0500

    FE: Store configuration API code splitting

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { transactionsApi } from './apiSlice';
+import { transactionsApi } from './transactionsApi';
+import { emptySplitApi } from './emptySplitApi';
 
 /**
  * Create a Redux store using configureStore, specifying the root reducer and extending the middleware stack to include additional middleware related to API handling
  * This is an initial set up for redux toolkit, which we will pass in the top level component (main.tsx)
  */
 export const store = configureStore({
-  reducer: { [transactionsApi.reducerPath]: transactionsApi.reducer },
+  reducer: { [emptySplitApi.reducerPath]: emptySplitApi.reducer },
   middleware: (getDefault) => getDefault().concat(transactionsApi.middleware),
 });
