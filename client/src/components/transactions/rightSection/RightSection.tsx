f6ac06a6c3752bfed2612c56e291e0ba102fa88a
vtran <vytran1805@gmail.com>
2024-01-03 17:40:32 -0500

    Improve logic of RightSection

--- client/src/components/transactions/rightSection/RightSection.tsx
+++ client/src/components/transactions/rightSection/RightSection.tsx
@@ -1,18 +1,26 @@
-import { TransactionsResponse } from '@/redux/types';
-import React from 'react';
+import { TransactionData, TransactionsResponse } from '@/redux/types';
+import { List, ListItem } from '@mui/material';
 
 type Props = {
   transactionDetail?: TransactionsResponse;
 };
 export const RightSection = (props: Props) => {
   const { transactionDetail } = props;
+  if (!transactionDetail) {
+    return <></>;
+  }
+  const { _id, __v, ...payload } = transactionDetail; // payload will contains the remaining attributes of TransactionsResponse (exclude '_id' and, '__v')
+  const filteredKeys = Object.keys(payload); // The array of payload keys
+  console.log({ filteredKeys });
+
   return (
-    <>
-      <div>Name: {transactionDetail?.name}</div>
-      <div>Amount: {transactionDetail?.amount}</div>
-      <div>Description: {transactionDetail?.description}</div>
-      <div>Date: {transactionDetail?.date}</div>
-      <div>Category: {transactionDetail?.category}</div>
-    </>
+    <List>
+      {filteredKeys.map((key) => (
+        <ListItem key={key}>
+          <strong>{key}: </strong>
+          {payload[key as keyof TransactionData]}
+        </ListItem>
+      ))}
+    </List>
   );
 };
