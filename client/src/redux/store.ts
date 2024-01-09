0333c5b024a2f177953fbd392d6669fb9be722c3
vtran <vytran1805@gmail.com>
2024-01-09 00:52:44 -0500

    Correct redux store

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,12 +1,10 @@
 import { emptySplitApi } from './emptyApi';
 import { configureStore } from '@reduxjs/toolkit';
-import { expensesApi } from './expensesApi';
-
 /**
  * Create a Redux store using configureStore, specifying the root reducer and extending the middleware stack to include additional middleware related to API handling
  * This is an initial set up for redux toolkit, which we will pass in the top level component (main.tsx)
  */
 export const store = configureStore({
   reducer: { [emptySplitApi.reducerPath]: emptySplitApi.reducer },
-  middleware: (getDefault) => getDefault().concat(expensesApi.middleware),
+  middleware: (getDefault) => getDefault().concat(emptySplitApi.middleware),
 });
