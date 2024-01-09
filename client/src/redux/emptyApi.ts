9d087a0991bbcf3a312355004bfddf8266108f7d
vtran <vytran1805@gmail.com>
2024-01-09 00:45:56 -0500

    Add tag types

--- client/src/redux/emptyApi.ts
+++ client/src/redux/emptyApi.ts
@@ -4,5 +4,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 // initialize an empty api service that we'll inject endpoints into later as needed
 export const emptySplitApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  tagTypes: ['Expenses', 'Incomes'],
   endpoints: () => ({}),
 });
