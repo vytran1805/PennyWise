12574b2b25470e29e152d356fab66b1015ad4542
vtran <vytran1805@gmail.com>
2024-01-09 00:45:46 -0500

    Follow RTK naming convention

--- client/src/redux/expensesApi.ts
+++ client/src/redux/expensesApi.ts
@@ -5,25 +5,25 @@ const EXPENSES_URL = 'api/expenses/';
 export const expensesApi = emptySplitApi.injectEndpoints({
   // reducerPath: 'expenses',
   // baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
-  endpoints: (builder) => ({
-    createExpense: builder.mutation<void, { data: ExpenseData }>({
+  endpoints: (build) => ({
+    createExpense: build.mutation<void, { data: ExpenseData }>({
       query: ({ data }) => ({
         url: EXPENSES_URL, // Assuming API endpoint for updating a specific expense
         method: 'POST', // Use the appropriate HTTP method (PUT, PATCH, etc.) for updating
         body: data, // Send the updated data to the server
       }),
     }),
-    getAllExpenses: builder.query<ExpenseResponse[], void>({
+    getAllExpenses: build.query<ExpenseResponse[], void>({
       query: () => EXPENSES_URL,
     }),
-    deleteExpense: builder.mutation<void, { _id: string }>({
+    deleteExpense: build.mutation<void, { _id: string }>({
       query: ({ _id }) => ({
         url: EXPENSES_URL, // Assuming API endpoint for deleting a specific expense
         method: 'DELETE',
         body: { _id },
       }),
     }),
-    updateExpense: builder.mutation<void, { data: ExpenseResponse }>({
+    updateExpense: build.mutation<void, { data: ExpenseResponse }>({
       query: ({ data }) => ({
         url: EXPENSES_URL, // Assuming API endpoint for updating a specific expense
         method: 'PATCH', // Use the appropriate HTTP method (PUT, PATCH, etc.) for updating
--- client/src/redux/incomesApi.ts
+++ client/src/redux/incomesApi.ts
@@ -5,25 +5,25 @@ const INCOME_URL = 'api/incomes/';
 export const incomesApi = emptySplitApi.injectEndpoints({
   // reducerPath: 'incomes',
   // baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
-  endpoints: (builder) => ({
-    createIncome: builder.mutation<void, { data: IncomeData }>({
+  endpoints: (build) => ({
+    createIncome: build.mutation<void, { data: IncomeData }>({
       query: ({ data }) => ({
         url: INCOME_URL, // Assuming API endpoint for updating a specific income
         method: 'POST', // Use the appropriate HTTP method (PUT, PATCH, etc.) for updating
         body: data, // Send the updated data to the server
       }),
     }),
-    getAllIncomes: builder.query<IncomeResponse[], void>({
+    getAllIncomes: build.query<IncomeResponse[], void>({
       query: () => INCOME_URL,
     }),
-    deleteIncome: builder.mutation<void, { _id: string }>({
+    deleteIncome: build.mutation<void, { _id: string }>({
       query: ({ _id }) => ({
         url: INCOME_URL, // Assuming API endpoint for deleting a specific income
         method: 'DELETE',
         body: { _id },
       }),
     }),
-    updateIncome: builder.mutation<void, { data: IncomeResponse }>({
+    updateIncome: build.mutation<void, { data: IncomeResponse }>({
       query: ({ data }) => ({
         url: INCOME_URL, // Assuming API endpoint for updating a specific income
         method: 'PATCH', // Use the appropriate HTTP method (PUT, PATCH, etc.) for updating
