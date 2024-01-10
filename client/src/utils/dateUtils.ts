96a1e25d79f31093cac42c217780122c602ac562
vtran <vytran1805@gmail.com>
2024-01-09 19:43:13 -0500

    Add sortDates util to sort dates correctly

--- client/src/utils/dateUtils.ts
+++ client/src/utils/dateUtils.ts
@@ -3,3 +3,13 @@ import moment from 'moment';
 export const dateFormat = (date: Date) => {
   return moment(date).format('MMM Do YYYY');
 };
+
+// This function will make sure Jan 2nd comes before Jan 10th
+export const sortDates = (dateStringA: string, dateStringB: string) => {
+  const dateA = new Date(dateStringA);
+  const dateB = new Date(dateStringB);
+  console.log({ dateA });
+  console.log({ dateB });
+
+  return dateA.getTime() - dateB.getTime();
+};
